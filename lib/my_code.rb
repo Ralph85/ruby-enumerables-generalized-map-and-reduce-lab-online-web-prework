7bb1c75ead37adede71eb3db2295f7183d995f00
Ralph Saint Albord <ralph.saintalbord@yahoo.com>
2019-07-26 21:08:06 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,9 +1,8 @@
 def make_sandwich(elelment1, element2)
   base = "A #{elelment1} and #{element2}"
-  puts base
-  yield
-  base
+  yield(base)
 end
 
-make_sandwich("chicken", "a sense of malaise") do |innards|
-  puts "making some tasty stuff..."
+make_sandwich("gator", "gumbo") do |innards|
+  "#{innards} on rye"
+end
